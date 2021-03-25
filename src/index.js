d37a8571d9c9315fd5790395a5f21b4349bb8602
Manit Kant <manit_kant@yahoo.com>
2021-03-25 23:05:03 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const {ApolloServer} = require('apollo-server');
+
+const typeDefs = `
+type Query {
+    info: String!,
+    feed: [Link!]!
+}
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
+}
+`
+
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+  }]
+
+const resolvers = {
+    Query: {
+        info: () => `This is the api of hackernews clone`,
+        feed: () => links,
+    },
+    Link:{
+        id: (parent) => parent.id,
+        description: (parent) => parent.description,
+        url: (parent) => parent.url
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+})
+
+
+server.listen().then(({url}) => console.log(`server is running on ${url}`));
+
